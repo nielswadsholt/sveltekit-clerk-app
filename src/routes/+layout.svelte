042fa7b465de2515922feac1292433015cc7301c
Niels Wadsholt <nwadsholt@ymail.com>
2024-05-26 18:30:42 +0200

    Add auth using clerk-sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import UserButton from 'clerk-sveltekit/client/UserButton.svelte'
+	import SignInButton from 'clerk-sveltekit/client/SignInButton.svelte'
+	import SignUpButton from 'clerk-sveltekit/client/SignUpButton.svelte'
+	import SignedIn from 'clerk-sveltekit/client/SignedIn.svelte'
+	import SignedOut from 'clerk-sveltekit/client/SignedOut.svelte'
+</script>
+
+<SignedIn>
+	<UserButton afterSignOutUrl="/" />
+</SignedIn>
+<SignedOut>
+    <SignInButton mode="modal" />
+    <SignUpButton mode="modal" />
+</SignedOut>
+
+<slot />
---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Admin Page</h1>
+<p>Only admins are allowed in here.</p>
