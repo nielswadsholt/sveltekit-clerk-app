60307912f87e1024fab52e0f7fbe1e464fa225b2
Niels Wadsholt <nwadsholt@ymail.com>
2024-09-22 23:54:06 +0200

    Use shared user page instead of hardcoded individual ones

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -3,7 +3,6 @@
 
 <h1>Site links:</h1>
 <div>
-    <div><a href="/alice">Alice's Page (only she should see this)</a></div>
-    <div><a href="/bob">Bob's Page (only he should see this)</a></div>
+    <div><a href="/user-page">User page displaying user-specific data</a></div>
 </div>
 
---
+++ src/routes/(sub-pages)/user-page/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import SignedIn from 'clerk-sveltekit/client/SignedIn.svelte';
+</script>
+
+<SignedIn let:user>
+    {#if !!user}
+        <h1>Welcome {user.fullName ?? user.primaryEmailAddress}!</h1>
+    {/if}
+</SignedIn>
