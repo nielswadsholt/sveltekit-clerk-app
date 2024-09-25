dd06e758f5532838fe9bc8477bd555a57b007422
Niels Wadsholt <nwadsholt@ymail.com>
2024-09-25 20:09:32 +0200

    Add admin link to main page

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -4,5 +4,6 @@
 <h1>Site links:</h1>
 <div>
     <div><a href="/user-page">User page displaying user-specific data</a></div>
+    <div><a href="/admin">Admin page (can only be viewed by admins)</a></div>
 </div>
 
